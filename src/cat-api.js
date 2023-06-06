a573e4edd56c9049e1e1086ce2bb8463b7d539ac
MHatalyak <matronahu@gmail.com>
2023-06-06 16:23:28 +0300

    edited files

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,43 +3,70 @@ const api_key =
   'live_EAPyDMjbUgKcfFUEpx7puTLp6nL8ysyeHlaXs7Dve77C0mnckEp2aPwOnY3nZbFL';
 let storedBreeds = [];
 
-fetch(url, {
-  headers: {
-    'x-api-key': api_key,
-  },
-})
-  .then(response => {
-    return response.json();
+function fetchBreeds() {
+  showLoader();
+
+  return fetch(url, {
+    headers: {
+      'x-api-key': api_key,
+    },
   })
-  .then(data => {
-    data = data.filter(img => img.image?.url != null);
+    .then(response => response.json())
+    .then(data => {
+      data = data.filter(img => img.image?.url != null);
+      storedBreeds = data;
 
-    storedBreeds = data;
+      const breedSelector = document.getElementById('breed_selector');
+      for (let i = 0; i < storedBreeds.length; i++) {
+        const breed = storedBreeds[i];
+        let option = document.createElement('option');
 
-    const breedSelector = document.getElementById('breed_selector');
-    for (let i = 0; i < storedBreeds.length; i++) {
-      const breed = storedBreeds[i];
-      let option = document.createElement('option');
+        if (!breed.image) continue;
 
-      if (!breed.image) continue;
+        option.value = i;
+        option.innerHTML = `${breed.name}`;
+        breedSelector.appendChild(option);
+      }
 
-      option.value = i;
-      option.innerHTML = `${breed.name}`;
-      breedSelector.appendChild(option);
-    }
-
-    showBreedInfo(0);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+      showBreedInfo(0);
+    })
+    .catch(handleError)
+    .finally(hideLoader);
+}
 
 function showBreedInfo(index) {
+  showLoader();
+
   const breed = storedBreeds[index];
   document.getElementById('breed_image').src = breed.image.url;
   document.getElementById('breed_name').textContent = breed.name;
   document.getElementById('breed_description').textContent = breed.description;
   document.getElementById('breed_temperament').textContent = breed.temperament;
+
+  hideLoader();
 }
 
-export { showBreedInfo };
+function showLoader() {
+  const loader = document.querySelector('.loader');
+  const catInfo = document.querySelector('.cat-info');
+
+  loader.classList.add('show');
+  catInfo.classList.add('hide');
+}
+
+function hideLoader() {
+  const loader = document.querySelector('.loader');
+  const catInfo = document.querySelector('.cat-info');
+
+  loader.classList.remove('show');
+  catInfo.classList.remove('hide');
+}
+function handleError() {
+  const errorElement = document.querySelector('.error');
+  const hideSpan = document.querySelector('.text');
+  hideSpan.classList.add('hide');
+  errorElement.classList.remove('hide');
+  errorElement.classList.add('show');
+}
+
+export { showBreedInfo, fetchBreeds, handleError };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
-import { showBreedInfo } from './cat-api.js';
+import { showBreedInfo, fetchBreeds, handleError } from './cat-api.js';
 
-const breedSelector = document.getElementById('breed_selector');
-breedSelector.addEventListener('change', function () {
-  const selectedIndex = this.value;
-  showBreedInfo(selectedIndex);
+document.addEventListener('DOMContentLoaded', function () {
+  const breedSelector = document.getElementById('breed_selector');
+  const loader = document.querySelector('.loader');
+  const catInfo = document.querySelector('.cat-info');
+  const errorElement = document.querySelector('.error');
+
+  breedSelector.addEventListener('change', function () {
+    const selectedIndex = this.value;
+    showBreedInfo(selectedIndex);
+  });
+
+  function showLoader() {
+    loader.classList.add('show');
+    loader.classList.remove('hide');
+    catInfo.classList.add('hide');
+    errorElement.classList.add('hide');
+  }
+
+  function hideLoader() {
+    loader.classList.remove('show');
+    loader.classList.add('hide');
+    catInfo.classList.remove('hide');
+  }
+
+  showLoader();
+
+  fetchBreeds().then(hideLoader).catch(handleError);
 });
