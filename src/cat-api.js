bcf82c6f4f4abf87ded74f74472cb7be5b6ab948
MHatalyak <matronahu@gmail.com>
2023-06-09 20:15:10 +0300

    edited

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,76 +1,18 @@
 const url = 'https://api.thecatapi.com/v1/breeds';
 const api_key =
   'live_EAPyDMjbUgKcfFUEpx7puTLp6nL8ysyeHlaXs7Dve77C0mnckEp2aPwOnY3nZbFL';
-let storedBreeds = [];
 
 function fetchBreeds() {
-  showLoader();
-
   return fetch(url, {
     headers: {
       'x-api-key': api_key,
     },
-  })
-    .then(response => response.json())
-    .then(data => {
-      data = data.filter(img => img.image?.url != null);
-      storedBreeds = data;
-
-      const breedSelector = document.getElementById('breed_selector');
-      for (let i = 0; i < storedBreeds.length; i++) {
-        const breed = storedBreeds[i];
-        let option = document.createElement('option');
-
-        if (!breed.image) continue;
-
-        option.value = i;
-        option.innerHTML = `${breed.name}`;
-        breedSelector.appendChild(option);
-      }
-
-      //   showBreedInfo(0);
-      const hideSpan = document.querySelector('.text');
-      hideSpan.classList.add('hide');
-    })
-    .catch(handleError)
-    .finally(hideLoader);
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error();
+    }
+    return response.json();
+  });
 }
 
-function showBreedInfo(index) {
-  showLoader();
-
-  const breed = storedBreeds[index];
-  document.getElementById('breed_image').src = breed.image.url;
-  document.getElementById('breed_name').textContent = breed.name;
-  document.getElementById('breed_description').textContent = breed.description;
-  document.getElementById('breed_temperament').textContent = breed.temperament;
-
-  hideLoader();
-  const hideSpan = document.querySelector('.text');
-  hideSpan.classList.remove('hide');
-}
-
-function showLoader() {
-  const loader = document.querySelector('.loader');
-  const catInfo = document.querySelector('.cat-info');
-
-  loader.classList.add('show');
-  catInfo.classList.add('hide');
-}
-
-function hideLoader() {
-  const loader = document.querySelector('.loader');
-  const catInfo = document.querySelector('.cat-info');
-
-  loader.classList.remove('show');
-  catInfo.classList.remove('hide');
-}
-function handleError() {
-  const errorElement = document.querySelector('.error');
-  const hideSpan = document.querySelector('.text');
-  hideSpan.classList.add('hide');
-  errorElement.classList.remove('hide');
-  errorElement.classList.add('show');
-}
-
-export { showBreedInfo, fetchBreeds, handleError };
+export { fetchBreeds };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import { showBreedInfo, fetchBreeds, handleError } from './cat-api.js';
+import { fetchBreeds } from './cat-api.js';
 
 document.addEventListener('DOMContentLoaded', function () {
   const breedSelector = document.getElementById('breed_selector');
   const loader = document.querySelector('.loader');
   const catInfo = document.querySelector('.cat-info');
   const errorElement = document.querySelector('.error');
+  const hideSpan = document.querySelector('.text');
+  let storedBreeds = [];
 
   breedSelector.addEventListener('change', function () {
     const selectedIndex = this.value;
     showBreedInfo(selectedIndex);
   });
+  function showBreedInfo(index) {
+    showLoader();
+
+    const breed = storedBreeds[index];
+    document.getElementById('breed_image').src = breed.image.url;
+    document.getElementById('breed_name').textContent = breed.name;
+    document.getElementById('breed_description').textContent =
+      breed.description;
+    document.getElementById('breed_temperament').textContent =
+      breed.temperament;
+
+    hideLoader();
+    hideSpan.classList.remove('hide');
+  }
 
   function showLoader() {
     loader.classList.add('show');
@@ -23,8 +39,34 @@ document.addEventListener('DOMContentLoaded', function () {
     loader.classList.add('hide');
     catInfo.classList.remove('hide');
   }
+  function handleError() {
+    hideSpan.classList.add('hide');
+
+    errorElement.classList.remove('hide');
+    errorElement.classList.add('show');
+  }
 
   showLoader();
 
-  fetchBreeds().then(hideLoader).catch(handleError);
+  fetchBreeds()
+    .then(data => {
+      console.log(data);
+      storedBreeds = data;
+
+      for (let i = 0; i < storedBreeds.length; i++) {
+        const breed = storedBreeds[i];
+        let option = document.createElement('option');
+
+        if (!breed.image) continue;
+
+        option.value = i;
+        option.innerHTML = `${breed.name}`;
+        breedSelector.appendChild(option);
+      }
+
+      //   showBreedInfo(0);
+      hideSpan.classList.add('hide');
+    })
+    .catch(handleError)
+    .finally(hideLoader);
 });
